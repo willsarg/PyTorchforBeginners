bfcdd9ad15e1209c8066b16d8d9ac012857ddb2e
Will Sergeant <12886992+willsarg@users.noreply.github.com>
2024-05-20 23:49:34 -0400

    create the LinearRegression py

---
+++ LinearRegressionModel.py
@@ -0,0 +1,6 @@
+#Import the Main PyTorch Library
+import torch
+#Import the PyTorch Neural Network Library
+import torch.nn as nn
+#Import the PyTorch Optimizer Library (For Training the Model)
+import torch.optim as optim
